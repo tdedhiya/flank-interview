6ca333463b0413f037dd84280aa94ab06a7446c8
Tejas Dedhiya <92185187+tdedhiya@users.noreply.github.com>
2023-09-28 15:21:40 -0700

    Update App.js

--- flank-interview-fe/src/App.js
+++ flank-interview-fe/src/App.js
@@ -6,38 +6,45 @@ import objective from './objective.js';
 import rehypeRaw from 'rehype-raw';
 
 const options = [
-    { value: null, label: 'All' },
-    { value: 100, label: '$100' },
-    { value: 200, label: '$200' },
-    { value: 300, label: '$300' },
-    { value: 400, label: '$400' },
-    { value: 500, label: '$500' },
-]
+  { value: '$100', label: '$100' },
+  { value: '$200', label: '$200' },
+  { value: '$300', label: '$300' },
+  { value: '$400', label: '$400' },
+  { value: '$500', label: '$500' },
+];
 
 function App() {
-    const [selected, setSelected] = useState([]);
+  const [selected, setSelected] = useState([]);
 
-    return (
-        <ThinTemplate instructionTitle="Front-end Interview">
-            <div class='options'>
-                {options.map((option) => (
-                    <button
-                        type='button'
-                        key={option.value}
-                    >
-                        {option.label}
-                    </button>
-                ))}
-            </div>
+  const handleButtonClick = (value) => {
+    if (selected.includes(value)) {
+      setSelected(selected.filter((item) => item !== value));
+    } else {
+      setSelected([value]);
+    }
+  };
 
-            <ReactMarkdown rehypePlugins={[rehypeRaw]} className='markdown' children={objective} />
+  return (
+    <ThinTemplate instructionTitle="Front-end Interview">
+      <div className="options">
+        {options.map((option) => (
+          <button
+            type="button"
+            key={option.value}
+            className={selected.includes(option.value) ? 'selected' : ''}
+            onClick={() => handleButtonClick(option.value)}
+          >
+            {option.label}
+          </button>
+        ))}
+      </div>
 
-            <h2>Selected State</h2>
-            <pre>
-                {JSON.stringify(selected, null, 2)}
-            </pre>
-        </ThinTemplate>
-    );
+      <ReactMarkdown rehypePlugins={[rehypeRaw]} className="markdown" children={objective} />
+
+      <h2>Selected State</h2>
+      <pre>{JSON.stringify(selected, null, 2)}</pre>
+    </ThinTemplate>
+  );
 }
 
 export default App;
