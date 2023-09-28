2b521e88cd41c7920253e98e4479fe9b55b059c3
Tejas Dedhiya <92185187+tdedhiya@users.noreply.github.com>
2023-09-28 16:13:12 -0700

    Update App.js

--- flank-interview-fe/src/App.js
+++ flank-interview-fe/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './app.scss';
 import ThinTemplate from './templates/ThinTemplate/thin-template';
 import ReactMarkdown from 'react-markdown';
@@ -6,6 +6,7 @@ import objective from './objective.js';
 import rehypeRaw from 'rehype-raw';
 
 const options = [
+  { value: 'All', label: 'All' },
   { value: '$100', label: '$100' },
   { value: '$200', label: '$200' },
   { value: '$300', label: '$300' },
@@ -17,21 +18,45 @@ function App() {
   const [selected, setSelected] = useState([]);
 
   const handleButtonClick = (value) => {
-    if (selected.includes(value)) {
-      setSelected(selected.filter((item) => item !== value));
+    if (value === 'All') {
+      setSelected(['All']);
     } else {
-      setSelected([value]);
+      if (selected.includes('All')) {
+        setSelected([value]);
+      } else {
+        if (selected.includes(value)) {
+          setSelected(selected.filter((item) => item !== value));
+        } else {
+          const selectedValues = selected.map((item) => parseInt(item.slice(1)));
+          selectedValues.push(parseInt(value.slice(1))); // Convert value to integer
+          const min = Math.min(...selectedValues);
+          const max = Math.max(...selectedValues);
+
+          // Create a range of selected items
+          const range = Array.from({ length: max - min + 1 }, (_, i) => `$${min + i}`);
+          setSelected(range);
+        }
+      }
     }
   };
 
   return (
     <ThinTemplate instructionTitle="Front-end Interview">
+      {/* FilterSelection component */}
       <div className="options">
         {options.map((option) => (
           <button
             type="button"
             key={option.value}
-            className={selected.includes(option.value) ? 'selected' : ''}
+            className={
+              selected.includes(option.value)
+                ? option.value === 'All'
+                  ? 'selected all'
+                  : selected.length === 1
+                  ? 'selected'
+                  : 'selected bidirectional-range'
+                : ''
+            }
             onClick={() => handleButtonClick(option.value)}
           >
             {option.label}
